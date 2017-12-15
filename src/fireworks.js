3e556117eacf39c509f28e0391f94ad5b4bbd3d3
Kent C. Dodds <kent+github@doddsfamily.us>
2017-12-14 23:46:21 -0700

    Woo

---
+++ src/fireworks.js
@@ -0,0 +1,196 @@
+import React from 'react'
+
+// Thanks Tim! https://codepen.io/timohausmann/pen/mGHbq?page=1&
+class Fireworks extends React.Component {
+  componentDidMount() {
+    const canvas = this.canvas
+    const ctx = canvas.getContext('2d')
+    let width = canvas.width
+    let height = canvas.height
+    let lastUpdate = new Date()
+    let mouseUpdate = new Date()
+    let lastMouse = []
+    const particles = []
+
+    function randMinMax(min, max, round) {
+      var val = min + Math.random() * (max - min)
+
+      return val
+    }
+
+    const TO_RAD = Math.PI / 180
+
+    function getAngle(x1, y1, x2, y2) {
+      var dx = x1 - x2
+      const dy = y1 - y2
+
+      return Math.atan2(dy, dx)
+    }
+
+    function getDistance(x1, y1, x2, y2) {
+      var xs = x2 - x1,
+        ys = y2 - y1
+
+      xs *= xs
+      ys *= ys
+
+      return Math.sqrt(xs + ys)
+    }
+
+    setTimeout(function() {
+      setFullscreen()
+
+      for (var i = 0; i < 40; i = i + 1) {
+        ;(function(i) {
+          setTimeout(function() {
+            createParticle({
+              x: width / 2 - 150 + i / 40 * 300,
+              degree: randMinMax(225, 135),
+            })
+          }, i * 20)
+        })(i)
+      }
+
+      loop()
+    }, 100)
+
+    canvas.addEventListener('mousemove', function(e) {
+      var thisUpdate = new Date(),
+        thisMouse = [e.offsetX || e.layerX, e.offsetY || e.layerY],
+        degree = 0,
+        distance = 0,
+        hue = 180
+
+      if (thisUpdate - mouseUpdate < 20) return
+
+      degree = getAngle(thisMouse[0], thisMouse[1], lastMouse[0], lastMouse[1])
+      degree /= TO_RAD
+      degree += 180
+
+      distance = getDistance(
+        thisMouse[0],
+        thisMouse[1],
+        lastMouse[0],
+        lastMouse[1],
+      )
+      if (distance < 1) distance = 1
+
+      hue = 180 * (degree / 360)
+
+      createParticle({
+        color: 'hsla(' + hue + ', 100%, 50%, ' + Math.random().toFixed(2) + ')',
+        x: thisMouse[0],
+        y: thisMouse[1],
+        degree: degree,
+        speed: distance * 20,
+        vs: -distance / 3,
+      })
+
+      mouseUpdate = thisUpdate
+      lastMouse = thisMouse
+    })
+    canvas.addEventListener('mousedown', function(e) {
+      for (var i = 0; i < 40; i = i + 1) {
+        createParticle({
+          x: e.offsetX || e.layerX,
+          y: e.offsetY || e.layerY,
+          color:
+            'hsla(' +
+            randMinMax(290, 360) +
+            ', 100%, 50%, ' +
+            Math.random().toFixed(2) +
+            ')',
+          speed: randMinMax(100, 350),
+        })
+      }
+    })
+    addEventListener('resize', setFullscreen)
+
+    function createParticle(args) {
+      var options = {
+        x: width / 2,
+        y: height / 2,
+        color:
+          'hsla(' +
+          randMinMax(160, 290) +
+          ', 100%, 50%, ' +
+          Math.random().toFixed(2) +
+          ')',
+        degree: randMinMax(0, 360),
+        speed: randMinMax(200, 250),
+        vd: randMinMax(-90, 90),
+        vs: randMinMax(-8, -5),
+      }
+
+      for (var key in args) {
+        options[key] = args[key]
+      }
+
+      particles.push(options)
+    }
+
+    function loop() {
+      var thisUpdate = new Date(),
+        delta = (lastUpdate - thisUpdate) / 1000,
+        amount = particles.length,
+        size = 2,
+        i = 0,
+        p
+
+      ctx.fillStyle = 'rgba(0,0,0,0.25)'
+      ctx.fillRect(0, 0, width, height)
+
+      for (; i < amount; i = i + 1) {
+        p = particles[i]
+
+        p.degree += p.vd * delta
+        p.speed += p.vs
+        if (p.speed < 0) continue
+
+        p.x += Math.cos(p.degree * TO_RAD) * (p.speed * delta)
+        p.y += Math.sin(p.degree * TO_RAD) * (p.speed * delta)
+
+        if (p.x < 0 || p.x > width) {
+          p.degree = 180 - p.degree
+        }
+        if (p.y < 0 || p.y > height) {
+          p.degree *= -1
+        }
+
+        ctx.save()
+
+        ctx.translate(p.x, p.y)
+        ctx.rotate(p.degree * TO_RAD)
+
+        ctx.fillStyle = p.color
+        ctx.fillRect(-size, -size, size * 2, size * 2)
+
+        ctx.restore()
+      }
+
+      lastUpdate = thisUpdate
+
+      requestAnimationFrame(loop)
+    }
+
+    function setFullscreen() {
+      width = canvas.width = innerWidth * 0.8
+      height = canvas.height = innerHeight * 0.8
+    }
+    this.cleanup = () => {
+      removeEventListener('resize', setFullscreen)
+    }
+  }
+  componentWillUnmount() {
+    this.cleanup()
+  }
+  render() {
+    return (
+      <div>
+        <canvas ref={c => (this.canvas = c)} />
+      </div>
+    )
+  }
+}
+
+export default Fireworks
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import {render} from 'react-dom'
-import {Motion, spring} from 'react-motion'
+import 'animate.css'
+import Fireworks from './fireworks'
+
+const inCondSandbox = true
 
 class App extends React.Component {
   static words = ['cat', 'becca', 'dodds', 'truck', 'horse', 'love', 'kindness']
@@ -14,8 +17,23 @@ class App extends React.Component {
   }
   state = this.initialState
   handleInputChange = e => {
-    if (e.target.value === this.state.currentWord) {
-      this.setState({inputValue: e.target.value})
+    const fullValue = `${this.state.inputValue}${e.target.value}`
+    if (this.state.inputValue === this.state.currentWord) {
+      // ignore... We're animating
+    } else if (this.state.currentWord.startsWith(fullValue)) {
+      this.setState({inputValue: fullValue})
+    } else {
+      this.nope()
+    }
+  }
+  nope() {
+    this.handleAnimateNopeRest = () => {
+      this.setState({animateNope: false})
+    }
+    this.setState({animateNope: true})
+  }
+  advanceIfMatches = () => {
+    if (this.state.inputValue === this.state.currentWord) {
       const {completed, todo, currentWord} = this.state
       this.advanceWord({
         completed: [...completed, currentWord],
@@ -23,21 +41,12 @@ class App extends React.Component {
         todo: todo.slice(1),
         inputValue: '',
       })
-    } else if (this.state.currentWord.startsWith(e.target.value)) {
-      this.setState({inputValue: e.target.value})
-    } else {
-      this.nope()
     }
-  }
-  nope() {
-    this.handleAnimateNopeFinished = () => {
-      this.setState({animateNope: false})
-    }
-    this.setState({animateNope: true})
   }
   advanceWord(stateToSet) {
     this.handleAnimateFinishedRest = () => {
       this.setState({...stateToSet, animateFinished: false})
+      this.input && this.input.focus()
     }
     this.setState({
       animateFinished: true,
@@ -45,7 +54,7 @@ class App extends React.Component {
   }
   reset = () => {
     this.setState(this.initialState)
-    this.input.focus()
+    this.input && this.input.focus()
   }
   render() {
     const {
@@ -56,67 +65,190 @@ class App extends React.Component {
       animateFinished,
       animateNope,
     } = this.state
+    if (!currentWord) {
+      return (
+        <div style={{textAlign: 'center'}}>
+          <h1>🎉 Congratulations! 🎊</h1>
+          <div>You're done!</div>
+          <div>
+            <button autoFocus={!inCondSandbox} onClick={this.reset}>
+              Start again
+            </button>
+          </div>
+          <Fireworks />
+        </div>
+      )
+    }
     return (
-      <div>
+      <div style={{textAlign: 'center', fontSize: 30}}>
         <h1>Typing for Becca</h1>
         <div>Let's get typing!</div>
         <div>Try typing the word:</div>
-        <div>
-          {animateNope ? (
-            <Motion
-              defaultStyle={{x: 0}}
-              style={{x: spring(1)}}
-              onRest={this.handleAnimateNopeFinished}
+        <div
+          style={{
+            width: 250,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            marginBottom: 20,
+            textAlign: 'left',
+          }}
+        >
+          <h2>
+            <Animated
+              onRest={this.handleAnimateNopeRest}
+              animation={
+                animateNope ? (
+                  'headShake'
+                ) : animateFinished ? (
+                  getRandomOutAnimation()
+                ) : (
+                  ''
+                )
+              }
+              style={{marginLeft: 10}}
             >
-              {({x}) => (
-                <h2
-                  style={{
-                    transform: `translate(${Math.pow(Math.E, -x) *
-                      Math.cos(2 * Math.PI * x) *
-                      10 -
-                      5}px)`,
-                  }}
-                >
-                  {currentWord}
-                </h2>
-              )}
-            </Motion>
-          ) : (
-            <h2>{currentWord}</h2>
-          )}
-        </div>
-        <div>
-          {animateFinished ? (
-            <Motion
-              defaultStyle={{x: 0}}
-              style={{x: spring(1)}}
-              onRest={this.handleAnimateFinishedRest}
-            >
-              {value => (
-                <h2
-                  style={{
-                    display: 'inline-block',
-                    transform: `rotate(${value.x}turn)`,
-                  }}
+              <span style={{color: 'blue'}}>{inputValue}</span>
+              {currentWord.slice(inputValue.length)}
+            </Animated>
+          </h2>
+          <div
+            style={{
+              border: '1px solid',
+              padding: 10,
+              display: 'flex',
+            }}
+            onClick={() => this.input.focus()}
+          >
+            <h2 style={{display: 'inline-block', margin: 0}}>
+              {animateFinished ? (
+                <Animated
+                  onRest={this.handleAnimateFinishedRest}
+                  animation={getRandomOutAnimation()}
+                  style={{display: 'inline-block'}}
                 >
                   {inputValue}
-                </h2>
+                </Animated>
+              ) : (
+                inputValue.split('').map((c, i) => (
+                  <InputChar key={i} onRest={this.advanceIfMatches}>
+                    {c}
+                  </InputChar>
+                ))
               )}
-            </Motion>
-          ) : (
-            <h2 style={{display: 'inline-block'}}>{inputValue}</h2>
-          )}
+            </h2>
+            <input
+              value={''}
+              autoFocus={!inCondSandbox}
+              ref={i => (this.input = i)}
+              onChange={this.handleInputChange}
+              style={{
+                flex: 1,
+                width: '100%',
+                border: 'none',
+                fontSize: '1.5em',
+                outline: 'none',
+              }}
+            />
+          </div>
         </div>
-        <input
-          ref={i => (this.input = i)}
-          value={inputValue}
-          onChange={this.handleInputChange}
-        />
-        <button onClick={this.reset}>Reset</button>
-        <pre>{JSON.stringify(this.state, null, 2)}</pre>
+        <button onClick={this.reset}>Start Over</button>
       </div>
     )
   }
 }
 
+class InputChar extends React.Component {
+  animation = getRandomInAnimation()
+  render() {
+    return (
+      <Animated
+        style={{display: 'inline-block'}}
+        animation={this.animation}
+        {...this.props}
+      />
+    )
+  }
+}
+
+function getRandomOutAnimation() {
+  const outAnimations = [
+    'bounceOut',
+    'bounceOutDown',
+    'bounceOutLeft',
+    'bounceOutRight',
+    'bounceOutUp',
+    'fadeOut',
+    'fadeOutDown',
+    'fadeOutLeft',
+    'fadeOutRight',
+    'fadeOutUp',
+    'flipOutX',
+    'flipOutY',
+    'hinge',
+    'lightSpeedOut',
+    'rollOut',
+    'rotateOut',
+    'rotateOutDownLeft',
+    'rotateOutDownRight',
+    'rotateOutUpLeft',
+    'rotateOutUpRight',
+    'slideOutDown',
+    'slideOutLeft',
+    'slideOutRight',
+    'slideOutUp',
+    'zoomOut',
+    'zoomOutDown',
+    'zoomOutLeft',
+    'zoomOutRight',
+    'zoomOutUp',
+  ]
+  return outAnimations[Math.floor(Math.random() * outAnimations.length)]
+}
+
+function getRandomInAnimation() {
+  const inAnimations = [
+    'fadeIn',
+    'fadeInDown',
+    'fadeInLeft',
+    'fadeInRight',
+    'fadeInUp',
+    'flipInX',
+    'flipInY',
+    'jackInTheBox',
+    'lightSpeedIn',
+    'rollIn',
+    'rotateIn',
+    'rotateInDownLeft',
+    'rotateInDownRight',
+    'rotateInUpLeft',
+    'rotateInUpRight',
+    'slideInDown',
+    'slideInLeft',
+    'slideInRight',
+    'slideInUp',
+    'zoomIn',
+  ]
+  return inAnimations[Math.floor(Math.random() * inAnimations.length)]
+}
+
+class Animated extends React.Component {
+  static defaultProps = {
+    onRest: () => {},
+  }
+  componentDidMount() {
+    this.div.addEventListener('animationend', () => {
+      this.props.onRest()
+    })
+  }
+  render() {
+    const {onRest, animation, ...rest} = this.props
+    return (
+      <div
+        className={`animated ${animation}`}
+        ref={div => (this.div = div)}
+        {...rest}
+      />
+    )
+  }
+}
 render(<App />, document.getElementById('root'))
